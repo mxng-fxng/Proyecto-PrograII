445d051655a5534bc656059334741f0a92cb0677
mxng-fxng <mingfang1304@gmail.com>
2025-05-26 17:10:55 +0200

    Casilla.java

--- src/hl/Casilla.java
+++ src/hl/Casilla.java
@@ -1,22 +1,28 @@
 package hl;
 
 public class Casilla {
-    private int color;
+    private int color; // atributo privado tipo entero
+
+    Casilla (int color){
+        this.color = color;
+    }
+
     /** Cambiar el color de ‘esta‘ casilla
     *
     * Alternando entre el blanco y el negro, codificados respectivamente por 0 y 1.
     */
     public void cambiarColor (){
-        if (color == 0)
-            color = 1;
-        color = 0;
+        if(this.color == 0) this.color = 1;
+        else if(this.color == 1) this.color = 0;
+        else throw new IllegalArgumentException("El color debe ser 0 o 1");
     }
+
 
     /** El color de ‘esta‘ casilla
     *
     * @return el numero de color de la casilla 0 para blanco, 1 para negro
     */
     public int color(){
-        return color;
+        return this.color;
     }
 }
